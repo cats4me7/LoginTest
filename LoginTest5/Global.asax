755518b87d64a6d5ca15a34ff0c9490b6ec0f9bc
cats4me7 <cats4me77@yahoo.com>
2017-11-01 12:36:36 -0400

    Add project files.

---
+++ LoginTest5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginTest5.MvcApplication" Language="C#" %>
---
+++ LoginTest5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginTest5.Startup))]
+namespace LoginTest5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
